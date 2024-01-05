fa74a98c29fb6119bdbfc0ec7a03a9380279dde0
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2024-01-05 17:30:37 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 1211;
+let accountEmail = "sujanmagar@gmail.com";
+var accountPassword = "12345";
+accountCity = "pokhara"
+let accountState;
+
+/*
+prefer not to use
+var variable because of issue in block scope and functional scope
+*/
+
+// accountId= 3;  // not allowed (const vaariable can not be changed)
+
+accountEmail = "sujan122@gmail.com"
+
+accountPassword= "3243"
+
+//console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity,accountState])
