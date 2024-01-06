fb1fc57772f1888057c4377fb7b8700862196fe3
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2024-01-06 15:26:03 +0000

    container config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,52 @@
+// let score = "44cds";
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "455" => 455
+// "44cds" => NaN
+// true => 1; false => 0;
+
+// let isLogined = 1;
+// let booleanIsLoginedIn = Boolean(isLogined);
+// console.log(booleanIsLoginedIn);
+
+// 1 => true; 0 => false;
+// " " => false;
+// "sujan" => true;
+
+// let someNumber = 6789;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********** Operation *********
+
+// let value = 9;
+// let negValue = -value;
+
+//  console.log(negValue);
+
+// console.log(19%6);
+
+// let str1 = "sujan";
+
+// let str2 = "magar";
+
+// let str3 = str1+ str2;
+// console.log(str3);
+
+// console.log(1+ "4");
+// console.log("1" + 4);
+// console.log("1" + 3 +3);
+// console.log(2 + 2 + "1");
+
+// console.log(+true);
+// console.log(+"");
+
+let x=4;
+let y= ++x;
+console.log(y);
