b50b375b4f9a0026472c5e7198933c466b6b2db8
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2024-01-06 17:01:19 +0000

    container config done

--- 01_basics/datatypeSummary.js
+++ 01_basics/datatypeSummary.js
@@ -16,18 +16,40 @@ const bigNumber = 34653535365435435434n;
 // reference type
  // array, object, functions...
 
- const countries = ["nepal", "China", "Pakistan"];
- console.log(countries);
- console.log(countries[1]);
+//  const countries = ["nepal", "China", "Pakistan"];
+//  console.log(countries);
+//  console.log(countries[1]);
 
- let myObj = {
-    name:"sujan",
-    roll:23
+//  let myObj = {
+//     name:"sujan",
+//     roll:23
+//  }
+//  console.log(myObj);
+
+//  const myFunction =function() {
+//     console.log("Hello World")
+//  }
+//  myFunction()
+
+
+ // heap and stack 
+
+ // primitive => stack; 
+ // Reference => heap;
+
+ let myEmail = "sujanmagar@gmail.com";
+ let anotherEmail = myEmail;
+anotherEmail = "suzanmar@google.com";
+console.log(myEmail);
+ console.log(anotherEmail);
+
+ let userOne = {
+   name: "sujan",
+   roll :3
  }
- console.log(myObj);
 
- const myFunction =function() {
-    console.log("Hello World")
- }
- myFunction()
-
+ let userTwo = userOne;
+  
+ userTwo.email = "sujanmagar@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
