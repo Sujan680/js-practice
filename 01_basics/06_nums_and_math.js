ef8debafcb92cd1b5cb49bc9e8c8d1be9c336d8e
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2024-01-07 11:38:16 +0000

    day 2

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,30 @@
+// const score = 500;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));  // 100.00 like this
+
+// const otherNumber = 134.543;
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// *********************** maths***************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(2.53));
+// console.log(Math.floor(2.53)); // takes lowest value i.e. 2
+// console.log(Math.ceil(2.53)); // takes highest value i.e. 3
+// console.log(Math.floor(4.9));  // 4
+// console.log(Math.sqrt(9));
+// console.log(Math.max(1,2,3,4,5,6,7,9))
+// console.log(Math.min(1,2,3,4,5,6,7,9))
+
+console.log(Math.random());
+console.log(Math.random()*10);
