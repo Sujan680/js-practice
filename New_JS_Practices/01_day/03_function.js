7fa3e8fef66e2338ee431322097836356e5b7507
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2024-11-25 13:00:11 +0000

    FUnction in Js

---
+++ New_JS_Practices/01_day/03_function.js
@@ -0,0 +1,29 @@
+
+// Function in js
+
+function myFunction(){
+    let carName = "Volvo";
+    console.log(carName);
+    
+}
+myFunction();
+
+// function to compute product of two numbers
+
+function ProductofTwoNumbers(num1, num2){
+    let product = num1 * num2;
+    console.log("The product of two numbers are:", product);
+    
+}
+ProductofTwoNumbers(2,3);
+
+
+function myAge(age){
+
+let voteage = (age < 18 ) ? "too Young": " Enough to vote"
+
+console.log(voteage);
+}
+
+myAge(122)
+
