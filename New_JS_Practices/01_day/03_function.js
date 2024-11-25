dd75e827bd2629a13164ad28266a021636434210
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2024-11-25 13:25:21 +0000

    Objects in js

--- New_JS_Practices/01_day/03_function.js
+++ New_JS_Practices/01_day/03_function.js
@@ -27,3 +27,60 @@ console.log(voteage);
 
 myAge(122)
 
+// Asynchronous
+// console.log("Start");
+// setTimeout(() => console.log("End"), 1000)
+// console.log("This is a sujan");
+
+
+// Object in Js is an unordered collections of properties
+//  which can be changed, added, deleted and some are read only 
+
+const car = {
+    type: "Fiat",
+    model: "500",
+    color: "white"
+};
+console.log(typeof car); // object
+console.log(car);
+
+// Accessing object properties: two ways as
+console.log(car.type); 
+console.log(car["color"]);
+
+console.log(car.model);
+console.log(car.color);
+
+// Adding new properties
+car.madeIn = "English"
+console.log(car);
+
+
+// Object created using new keyword
+
+const person = new Object();
+
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 43;
+
+console.log(person["firstName"]);
+
+console.log(`${person.firstName} is ${person.age} years old`);
+
+// Nested objects
+myObj = {
+    name:"John",
+    age:30,
+    myCars: {
+      car1:"Ford",
+      car2:"BMW",
+      car3:"Fiat"
+    }
+  }
+
+  // accessing
+  console.log(myObj.myCars.car1);
+  console.log(myObj.myCars.car2);
+  console.log(myObj.myCars.car3);
+  
