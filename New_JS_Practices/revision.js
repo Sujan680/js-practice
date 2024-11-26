d68dd67782e9c0997baeeab06b432c3e0b9cfbad
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2024-11-26 10:24:40 +0000

    Revision of Js

---
+++ New_JS_Practices/revision.js
@@ -0,0 +1,38 @@
+
+console.log("Hello, this is a revision session of all js learned till now");
+
+const greeting = 'Hello';
+const name = "ALice";
+
+const message = greeting.concat(',', name);
+console.log(message);
+
+
+const txt = "Javascript is fun!";
+
+console.log(txt.search('is'));
+
+
+const text = 'I like cats. cats are great!';
+
+console.log(text.replaceAll("cats", 'Dogs'));
+
+
+const txt1 = "Javascript";
+
+console.log(txt1.slice(0,5));
+
+
+const text2 = '   Hello, World!   ';
+const trimmed = text2.trim();
+
+console.log(trimmed);
+
+
+const myText = "Javascript is fun";
+console.log(myText.includes("Python"));
+console.log(myText.includes("Javascript"));
+
+
+const fruit = "Apple";
+console.log(fruit.split());
