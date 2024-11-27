50adc9dfa1ec2afbc20172889d32214bc88c8ce6
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2024-11-27 12:24:45 +0000

    Some higehr order functions in js

--- New_JS_Practices/function.js
+++ New_JS_Practices/function.js
@@ -1,5 +1,6 @@
 
 // A javascript function is a block of a code designed to perform a particular TextTrackList. 
+// Re-use
 
 function walkForward() {
     console.log("The robot is walking forward.");
@@ -21,10 +22,12 @@ function greetUser() {
   farewellUser(); // Output: Goodbye, User!
 
 // Parameters and arguments
-function walkForward(steps) {
+// function parameters are like local variable of function which have block scope
+
+function walkForward(steps) { //steps is parameter
     console.log(`The robot walks ${steps} step forward`);
   }
-walkForward(4);
+walkForward(4); // 4 is arguments
 
 function greetUser(name,city) {
     console.log(`Hi, I am ${name} from ${city}.`);
@@ -47,13 +50,13 @@ function getGreeting(name) {
 }
 console.log(getGreeting("Sujan"));
 
-
+// to multiply two number
 function multiplyNumbers(a, b) {
     return a * b;
   }
 console.log(multiplyNumbers(4,5));
   
-
+// to check even
 const isEven = (number) => {
     return number % 2 == 0
 }
@@ -63,7 +66,6 @@ console.log(isEven(12)); // true
 let tool = "Wrench";
 function fixRobot() {
     console.log("Using", tool);
-    
 }
 fixRobot();
 
@@ -73,12 +75,12 @@ fixRobot();
 function myFunction() {
     let tool1 = "Screwdriver";
     console.log("Using", tool1);
-    
 }
 myFunction();
 // console.log(tool1); // Error: not defined
 
 
+// Global and local functions in js
 function outerFunction() {
     let outerTool = "Drill";
 
@@ -93,8 +95,90 @@ function outerFunction() {
 }
 
  outerFunction();
+ // in the above code innerfunction have access to both innerTool and outerTool
+ // but outerfunction does not have access to innerTool but access to outerTool 
 
 //  Arrow function 
 
 const sayHello = name => `Hello, ${name}`;
 console.log(sayHello("sujan")); // Hello, sujan
+
+
+function addNumbers(a,b,c) {
+    return a+b+c;
+}
+console.log("The addition of numbers are", addNumbers(1,2,3));
+
+
+// function expression 
+
+const add = function(a,b) {
+    return a+ b;
+    console.log("this is not executed") // This will not be executed
+}
+console.log("SUm is", add(4,5))
+
+
+// Arrow functions:: shorter way of writing functions
+
+const subtraction = (a,b) => a-b;
+console.log("Result is", subtraction(9,4));
+
+
+// to count the number of vowels in the string
+
+function countVoewls(str) {
+    let count = 0;
+    for (const char of str) {
+        if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u"){
+            count++;
+        }
+    }
+    console.log("The number of vowels are",count);
+}
+ countVoewls("i am fromm the top of the world");
+
+
+ // ForEach loop: arr.forEach(callback function(val, indx, arr))
+//  (Callback function): It is a function to execute for each element in the array
+ // A callback function is a function passed as an argument to another function
+
+ // this is how it is created ...
+
+//  arr1.forEach((val) => {
+//     console.log(val);
+//  })
+
+let arr = [3,7,9,10,34,22]
+
+arr.forEach((val) => {
+    console.log(val);
+})
+
+
+// Callback function example
+
+function greet(name, callback) {
+    console.log(`Hello, ${name}`);
+    callback(); // executes the callback function
+}
+
+// a simple callback function
+function sayGoodbye() {
+    console.log("Goodbye!"); 
+}
+
+// call the greet function with a callback
+greet("Sujan", sayGoodbye);
+
+const nums = [1,2,3,4,5,6,7];
+// nums.forEach((val) => {
+//     let square = val ** 2;
+//     console.log(square);
+// })
+
+const calcSquare = (val) => console.log(val * val);
+
+ nums.forEach(calcSquare);
+
+
