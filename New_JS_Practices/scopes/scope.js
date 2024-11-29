89b97e01e93a574782248c7fc791c258c9af302c
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2024-11-29 09:12:10 +0000

    IIFE and Callstack and global execution context in js

--- New_JS_Practices/scopes/scope.js
+++ New_JS_Practices/scopes/scope.js
@@ -44,5 +44,46 @@ console.log(addOne(5)); // hoisting
  
  const addTwo = function(num) {
     return num + 3;
+   }
+   addTwo(6);
+   
+//  Immediately Invoked Function Expression(IIFE) 
+
+(function () {
+   console.log("This is an IIFE");
+}) ();
+
+// or using arrow function 
+
+(() => {
+   console.log("This is an arrow function IIFE")
+}) ();
+
+// We use IIFE:
+// To protect variables from being accessed globally.
+// To execute code immediately while maintaining a clean namespace.
+
+( (name) => {
+   console.log(`My name is ${name}`);
+   
+}) ('sujan');
+
+// Callstack
+
+function first() {
+   console.log('First function start');
+   second();
+   console.log('First function end');
  }
- addTwo(6);
+ 
+ function second() {
+   console.log('Second function start');
+   third();
+   console.log('Second function end');
+ }
+ 
+ function third() {
+   console.log('Third function execution');
+ }
+ 
+ first();
